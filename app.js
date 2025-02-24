8efa96b0238481a5f51a53686992d9b4aefd3671
jfrv1 <jose_francisco38@hotmail.com>
2025-02-23 23:55:04 -0600

    se agrego una funcion para seleccionar el id=amigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function limpiarEspacio() {
+    document.getElementById("amigo").value = "";
+
+}
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.trim();
+    if (amigo === "") {
+        alert("Debe ingresar un nombre");
+        return;
+    } 
+}
